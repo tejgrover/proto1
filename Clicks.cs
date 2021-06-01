e9bdcc4628d6ff63b2e47147e707a6692197a142
Tej Grover <56784926+tejgrover@users.noreply.github.com>
2021-06-01 17:33:01 +0530

    Add files via upload

---
+++ Clicks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clicks : MonoBehaviour
+{
+    public int cli;
+    public Text clicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clicked.text = cli.ToString();
+    }
+
+    public void add()
+    {
+        if(cli < 36)
+            {
+            cli++;
+            }
+    }
+}
